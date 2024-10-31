08c9712a50b5b8008f55d32babd396bd9875f4f4
Glitch (political-trail-pick) <none>
2024-10-31 21:38:13 +0000

    🌋🕤 Updated by me

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme="blue">Hello</Button>;
+  return <Button colorScheme="blue">Hello  from Lagos</Button>;
 }
 
 export default App;
